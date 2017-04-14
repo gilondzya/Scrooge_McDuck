b497555c44d4dfb1f2ef5a836aaae7f7660d74b2
Liza <extraliz@yandex.ru>
2017-04-14 21:22:07 +0300

    Рассчёт суммы возможной ипотеки по ежемесячному пл

--- ипотека по ежемесячному платежу/ipoteka_po_ezhemes.cpp
+++ ипотека по ежемесячному платежу/ipoteka_po_ezhemes.cpp
@@ -12,12 +12,12 @@ stavka - годова€ процентна€ ставка, в коэффициентах (0,155 = 15,5% / 100%)
 summa - обща€ сумма займа 
 months - срок погашени€ в мес€цах 
 */
-void Ipoteka_po_ezhemes(long double ezhemes,double stavka,int months)
+long long double Ipoteka_po_ezhemes(long long double ezhemes,double stavka,int months)
 {
-  long double summa;
+  long long double summa;
   
   summa = ( ezhemes * (pow(stavka + 1, months) - 1) )/ ( stavka * pow(stavka + 1, months) ) ;
-  cout<<"ѕри ежемес€чном платеже "<<ezhemes<<" и сроке погашени€ "<<months<<" мес€цев можно вз€ть ипотеку суммой : "<<summa<<" рублей"<<endl;
+  return summa;
 }
 
 
@@ -27,22 +27,3 @@ double kstavka(double pr)
   return pr/100;       
 }
 
-int main()
-{
- setlocale(LC_ALL,"Russian"); 
- double Stavka = 12;
- double Y1=60; //5 лет
- double Y2=120;  //10 лет
- double Y3=240; //20 лет
- 
- long double Ezhemes;
- cout<< "¬ведите ежемес€чный платЄж : ";
- cin>>Ezhemes;
- 
- Ipoteka_po_ezhemes(Ezhemes,kstavka(Stavka)/12, Y1);
- Ipoteka_po_ezhemes(Ezhemes,kstavka(Stavka)/12, Y2);
- Ipoteka_po_ezhemes(Ezhemes,kstavka(Stavka)/12, Y3);
-    
-    system("PAUSE");
-    return EXIT_SUCCESS;
-}
